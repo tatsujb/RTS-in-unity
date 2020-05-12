b758f34d37d71c006e2a8c43c1e19d1e86c3ca3c
Embersands Game Studios <eitan.levy.159@gmail.com>
2020-05-12 18:55:05 +0300

    Added selection method

---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Transform selection = hit.transform;
+
+                Unit selectionUnit = selection.gameObject.GetComponent<Unit>();
+                if (selectionUnit != null)
+                {
+                    selectionUnit.SetSelection(true);
+                }
+            }
+        }
+    }
+}
