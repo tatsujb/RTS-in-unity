bd8839ae82ed27c6353726d09577773fb010a809
Embersands Game Studios <eitan.levy.159@gmail.com>
2020-05-12 17:30:57 +0300

    Added editor folder, witch was missing

---
+++ Assets/Editor/TerrainGeneratorEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TerrainGenerator terrain = (TerrainGenerator)target;
+
+        if (GUILayout.Button("Generate Terrain"))
+        {
+            terrain.Generate();
+        }
+    }   
+}
