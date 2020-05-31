34690ec62050b20a418f716f55d18916172681e1
3174N <63750650+3174N@users.noreply.github.com>
2020-05-31 18:19:26 +0300

    Added height to map editor

---
+++ Assets/Scripts/MapEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditor : MonoBehaviour
+{
+    #region variables
+    TerrainGenerator terrain;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<TerrainGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // terrain.Generate(true);
+    }
+
+    public void ChangeHeight(float amount)
+    {
+        terrain.height += amount;
+
+        terrain.Generate(true);
+    }
+}
